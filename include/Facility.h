7dc0062355f47e33467dede95d400ef1652eefaa
AsafNahmany7 <nahmany10012.net@gmail.com>
2024-11-27 07:44:33 +0000

    Completed facilities and settlements

--- include/Facility.h
+++ include/Facility.h
@@ -18,13 +18,31 @@ enum class FacilityCategory {
 
 class FacilityType {
     public:
-        FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
-        const string &getName() const;
-        int getCost() const;
-        int getLifeQualityScore() const;
-        int getEnvironmentScore() const;
-        int getEconomyScore() const;
-        FacilityCategory getCategory() const;
+        FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score) : name(name),category(category),price(price),lifeQuality_score(lifeQuality_score),economy_score(economy_score),environment_score(environment_score){};
+        
+        //Copy constructor for Facility class's 2nd constructor
+        FacilityType(const FacilityType& other):name(other.name),category(other.category),price(other.price),lifeQuality_score(other.lifeQuality_score),economy_score(other.economy_score),environment_score(other.environment_score){};
+
+
+        const string &getName() const {
+            return name;
+        }
+        int getCost() const {
+            return price;
+        }
+
+        int getLifeQualityScore() const {
+            return lifeQuality_score;
+        }
+        int getEnvironmentScore() const {
+            return environment_score;
+        }
+        int getEconomyScore() const {
+            return economy_score;
+        }
+        FacilityCategory getCategory() const {
+            return category;
+        }
 
     protected:
         const string name;
@@ -40,14 +58,83 @@ class FacilityType {
 class Facility: public FacilityType {
 
     public:
-        Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
-        Facility(const FacilityType &type, const string &settlementName);
-        const string &getSettlementName() const;
-        const int getTimeLeft() const;
-        FacilityStatus step();
-        void setStatus(FacilityStatus status);
-        const FacilityStatus& getStatus() const;
-        const string toString() const;
+    //First constructor: "From Zero"
+        Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
+        FacilityType(name,category,price,lifeQuality_score,economy_score,environment_score),settlementName(settlementName),status(FacilityStatus::UNDER_CONSTRUCTIONS),timeLeft(price){};
+    
+    //Second constructor: "Use instance of FacilityType"
+        Facility(const FacilityType &type, const string &settlementName) :
+        FacilityType(type),settlementName(settlementName),status(FacilityStatus::UNDER_CONSTRUCTIONS),timeLeft(price){};
+    
+    //Methods:
+
+        const string &getSettlementName() const {
+            return settlementName;
+        };
+
+
+        const int getTimeLeft() const {
+        return timeLeft;
+        };
+
+        FacilityStatus step(){
+
+            if(timeLeft == 0){
+                return status;
+            }
+            else{
+                timeLeft--;
+                if(timeLeft == 0){
+                    status = FacilityStatus::OPERATIONAL;
+                }
+
+                return status;
+            }
+        };
+
+        void setStatus(FacilityStatus status){
+            this->status = status;
+        }
+        const FacilityStatus& getStatus() const {
+            return status;
+        }
+
+
+        const string toString() {
+            string result = "Facility: " + getName() + "\n";
+            result += "Category: ";
+
+            if (getCategory() == FacilityCategory::LIFE_QUALITY) {
+                result += "Life Quality\n";
+            } 
+            else if (getCategory() == FacilityCategory::ECONOMY) {
+                 result += "Economy\n";
+            } 
+            else if (getCategory() == FacilityCategory::ENVIRONMENT) {
+                result += "Environment\n";
+            }
+
+            result += "Price: " + std::to_string(getCost()) + "\n";
+            result += "Life Quality Score: " + std::to_string(getLifeQualityScore()) + "\n";
+            result += "Economy Score: " + std::to_string(getEconomyScore()) + "\n";
+            result += "Environment Score: " + std::to_string(getEnvironmentScore()) + "\n";
+            result += "Settlement: " + getSettlementName() + "\n";
+
+            if (getStatus() == FacilityStatus::UNDER_CONSTRUCTIONS) {
+                result += "Status: Under Construction\n";
+            } 
+
+            else if (getStatus() == FacilityStatus::OPERATIONAL) {
+                result += "Status: Operational\n";
+            }
+
+            result += "Time Left: " + std::to_string(getTimeLeft()) + "\n";
+
+            return result;
+        };
+
+
+
 
     private:
         const string settlementName;
--- include/Settlement.h
+++ include/Settlement.h
@@ -14,12 +14,32 @@ enum class SettlementType {
 
 class Settlement {
     public:
-        Settlement(const string &name, SettlementType type);
-        const string &getName() const;
-        SettlementType getType() const;
-        const string toString() const;
+        Settlement(const string &name, SettlementType type) : name(name),type(type){};
+        const string &getName()const {
+            return name;
+        };
+        SettlementType getType()const {
+            return type;
+        };
+        const string toString(){
+            string output =  "Settlement Name: " + name + ", Type: " + settlementTypeToString(type);
+            return output;
+
+
+        };
 
         private:
             const string name;
             SettlementType type;
+
+            //Private method to determine the string value from the enum for toString
+            const string settlementTypeToString(SettlementType type) const {
+        
+                switch (type) {
+                    case SettlementType::VILLAGE: return "VILLAGE";
+                    case SettlementType::CITY: return "CITY";
+                    case SettlementType::METROPOLIS: return "METROPOLIS";
+                    default: return "UNKNOWN";
+                }
+            }
 };
