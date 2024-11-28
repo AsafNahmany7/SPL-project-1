d07a71fccac1a561df335f17ad06c8416a1789f9
ShohAmar <shohamar5@gmail.com>
2024-11-28 11:25:44 +0000

    sh

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -6,25 +6,29 @@
 using std::string;
 using std::to_string;
 
- Settlement::Settlement(const string &name, SettlementType type) : name(name),type(type){};
+    Settlement::Settlement(const string &name, SettlementType type) : name(name),type(type){};
+    
     const string& Settlement:: getName()const {
         int i =1;
         return name;
     };
-SettlementType Settlement::getType()const {
-    return type;
-};
-const string Settlement:: toString()const{
-    string output =  "Settlement Name: " + name + ", Type: " + settlementTypeToString(type);
-    return output;
+    
+    SettlementType Settlement::getType()const {
+        return type;
     };
-const string Settlement:: settlementTypeToString(SettlementType type) const {
+    
+    const string Settlement:: toString()const{
+        string output =  "Settlement Name: " + name + ", Type: " + settlementTypeToString(type);
+        return output;
+    };
+    
+    const string Settlement:: settlementTypeToString(SettlementType type) const {
         
     switch (type) {
         case SettlementType::VILLAGE: return "VILLAGE";
         case SettlementType::CITY: return "CITY";
         case SettlementType::METROPOLIS: return "METROPOLIS";
         default: return "UNKNOWN";
-                }
+    }
 }
 
