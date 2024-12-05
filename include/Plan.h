ff10c189d37e2f3dbd01263a465772e85bd3fb9c
ShohAmar <shohamar5@gmail.com>
2024-12-05 09:50:57 +0000

    Final-2

--- include/Plan.h
+++ include/Plan.h
@@ -16,6 +16,8 @@ class Plan {
         Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, const vector<FacilityType> &facilityOptions);
         Plan(const Plan& other);
         Plan& operator=(const Plan& other) = delete;
+        Plan(const Plan&& other) = delete;
+        Plan& operator=(const Plan&& other) = delete;
         ~Plan();
         
         int getlifeQualityScore() const;
